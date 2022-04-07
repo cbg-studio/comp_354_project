7f755c9ea96e993ea96b1cfef0e90854a47ea953
cbg-studio <47990933+cbg-studio@users.noreply.github.com>
2022-04-07 19:46:58 -0400

    Add files via upload

---
+++ src/main/java/com/example/cyclingapp/UserProfileActivity.java
@@ -0,0 +1,58 @@
+package com.example.cyclingapp;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NavUtils;
+import androidx.preference.PreferenceFragmentCompat;
+
+public class UserProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_profile);
+
+
+        if(savedInstanceState == null){
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.user_profile, new UserProfileFragment())
+                    .commit();
+        }
+
+        ActionBar actionBar = getSupportActionBar();
+
+        if (actionBar != null) {
+            actionBar.setTitle("User Profile");
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+
+        if(item.getItemId() == android.R.id.home){
+
+            NavUtils.navigateUpFromSameTask(this);
+            return true;
+        }
+        else
+            return super.onOptionsItemSelected(item);
+    }
+
+    public static class UserProfileFragment extends PreferenceFragmentCompat{
+
+        @Override
+        public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+            setPreferencesFromResource(R.xml.root_user_profile, rootKey);
+        }
+    }
+}
